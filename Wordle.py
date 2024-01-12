2584831f0c98ee12ee9af8767a6165411d4b3f16
jfietkau1 <120548552+jfietkau1@users.noreply.github.com>
2024-01-12 10:23:44 -0700

    milestone 2

--- Wordle.py
+++ Wordle.py
@@ -10,21 +10,28 @@ import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
+    #this function returns the word we just entered
+def get_word(window):
+    row = window.get_current_row()
+    current_word = window.get_square_letter(row, 0) + window.get_square_letter(row, 1) + window.get_square_letter(row, 2) + window.get_square_letter(row, 3) + window.get_square_letter(row, 4)
+    current_word = current_word.lower()
+    return current_word
+
+
 def wordle():
     gw = WordleGWindow()
     def enter_action(s):
-        current_word = gw.get_current_row()
-        print(current_word)
-        for i in FIVE_LETTER_WORDS:
-            if FIVE_LETTER_WORDS[i] == current_word:
-                realWordBool = True
-            else:
-                realWordBool = False
-        if realWordBool == True:
+        
+        word = get_word(gw) #calls the get_word function
+    
+        if word in FIVE_LETTER_WORDS: #shows the user a message of whether or not their word is actually a word
             gw.show_message("This is a word! But are you right??? (Milestone 2)")
         else:
             gw.show_message("This is not a word!  (Milestone 2)")
-            
+
+
+
+
     # # Gets a FIVE_LETTER_WORD from WordleDictionary
     # rand_num = random.randint(0, len(FIVE_LETTER_WORDS) - 1)
     # rand_word = FIVE_LETTER_WORDS[rand_num]
